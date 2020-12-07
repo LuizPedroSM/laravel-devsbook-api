43f1aad253133e53efcaac28f525ea5461947c25
Luiz Pedro <luizpsmoreira@gmail.com>
2020-12-07 18:07:25 -0300

    Criando as Rotas

--- routes/api.php
+++ routes/api.php
@@ -1,23 +1,31 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::get('/ping',  fn() => ['pong' => true]);
 
-Route::get('/ping', function () {
-    return ['pong' => true];
-});
+Route::get('/401', [AuthController::class, 'unauthorized'])->name('login');
 
-// Route::middleware('auth:api')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+Route::post('/auth/login', [AuthController::class, 'login']);
+Route::post('/auth/logout', [AuthController::class, 'logout']);
+Route::post('/auth/refresh', [AuthController::class, 'refresh']);
+
+Route::post('/user', [AuthController::class, 'create']);
+Route::put('/user', [UserController::class, 'update']);
+Route::get('/user', [UserController::class, 'read']);
+
+Route::post('/user/avatar', [UserController::class, 'updateAvatar']);
+Route::post('/user/cover', [UserController::class, 'updateCover']);
+Route::get('/user/{id}', [UserController::class, 'read']);
+
+Route::post('/feed', [FeedController::class, 'create']);
+Route::get('/feed', [FeedController::class, 'read']);
+
+Route::get('/feed/user', [FeedController::class, 'userFeed']);
+Route::get('/feed/user/{id}', [FeedController::class, 'userFeed']);
+
+
+Route::post('/post/{id}/like', [PostController::class, 'like']);
+Route::post('/post/{id}/comment', [PostController::class, 'comment']);
+
+Route::get('/search', [SearchController::class, 'search']);
